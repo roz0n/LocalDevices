79332891077912134057daf1143e9a1d728ec0f6
Arnold Rozon <arnold@rozon.org>
2024-04-23 20:13:31 -0700

    Initial Commit

---
+++ LocalDevices/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalDevices
+//
+//  Created by Arnaldo Rozon on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalDevices/LocalDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalDevicesApp.swift
+//  LocalDevices
+//
+//  Created by Arnaldo Rozon on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
